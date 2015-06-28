41b305d4fc9fc5335c89bc1e3b2e6b0491c7fd45
Kelvin <kelvin_guerrero@hotmail.com>
2015-06-27 20:37:23 -0500

    Generacion de cargue por archivos

--- loader/main.py
+++ loader/main.py
@@ -1,6 +1,6 @@
 from loadercoordinador import estudiantes, profesores
-import os
 import argparse
+
 __author__ = 'kelvin'
 
 
@@ -25,7 +25,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--load', default='', type=str, help='Type of load to do.')
     parser.add_argument('-f', '--ofile', default='', type=str, help='File to load.')
-    #parser.add_argument('-u','--utestlink', action="store_true", default=False, help='Should it be uploaded to TestLink?')
     args = parser.parse_args()
     print(args)
     main(**vars(args))
